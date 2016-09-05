f7bddb04eb503b5e9ce8a2fe1f87fc4e1db904e6
Andrew Palmer <andrew.palmer.10+github.personal@gmail.com>
2016-09-05 14:01:51 -0300

    Uses recursive search to find results node from user click

--- scripts/content.js
+++ scripts/content.js
@@ -7,23 +7,41 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
   style.type = 'text/css';
   style.href = chrome.extension.getURL('css/custom.css');
   (document.head || document.documentElement).appendChild(style);
-  const resultsList = document.querySelectorAll('li');
+  const resultsList = document.querySelectorAll('a');
   const imgSrc = chrome.extension.getURL('images/remove.png');
-
-  [].forEach.call(resultsList, function(header) {
-    header.style.position = 'relative';
-    const clearButton = document.createElement("img");
-    clearButton.setAttribute("class", "removeItem");
-    clearButton.setAttribute("title", "delete this result");
-    clearButton.src = imgSrc;
-    header.insertBefore(clearButton, header.children[0]);
-  });
 
   document.addEventListener('click', function(e) {
     e = e || window.event;
-    console.log(e.target.parentElement);
-    const unwantedResult = e.target.parentElement;
-    const allResults = e.target.parentElement.parentElement;
-    // allResults.removeChild(unwantedResult);
+    if(e.target.className === 'removeItem') {
+      let unwantedResult = e.target.parentElement;
+      let allResults = unwantedResult.parentElement;
+      allResults.removeChild(unwantedResult);
+    } else {
+      let element = e.target.parentElement;
+      getResultsList(element);
+    }
   }, false);
+
+  let getResultsList = function(element) {
+    let parentElement = element.parentElement;
+    let elementSiblings = element.parentElement.children;
+    let siblingsMatch = [].every.call(elementSiblings, function(currentElement) {
+      return currentElement.class === element.class;
+    });
+    if(siblingsMatch && elementSiblings.length > 3) {
+      // arbitrarily decide that a 'results list' should have more than 3 results... this will help
+      // prevent the searching algorithm from returning menu options or things like that, maybe?
+      const resultsList = elementSiblings;
+      [].forEach.call(resultsList, function(header) {
+        header.style.position = 'relative';
+        const clearButton = document.createElement("img");
+        clearButton.setAttribute("class", "removeItem");
+        clearButton.setAttribute("title", "delete this result");
+        clearButton.src = imgSrc;
+        header.insertBefore(clearButton, header.children[0]);
+      });
+    } else {
+      getResultsList(parentElement);
+    }
+  };
 });
--- scripts/extension.js
+++ scripts/extension.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {data: 'culled'}, function(response) {
-        $('#status').html('changed data in page');
+        $('#status').html('now click the results list to add a delete option');
+        button.parentElement.removeChild(button)
         console.log('success');
       });
     });
